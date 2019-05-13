0c40d11f09979731295857eb128ebfb9f031557e
thaotram <sawa.kisei@gmail.com>
2019-05-13 14:21:04 +0700

    Step 8.3

--- server/Controllers/DemoController.cs
+++ server/Controllers/DemoController.cs
@@ -1,35 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Realms;
+using server.Models;
 
 namespace server.Controllers
 {
     public class DemoController : BaseController
     {
+        private Realm realm => Realm.GetInstance();
+        private IQueryable<Post> list => realm.All<Post>();
+        private int NextId => list.Count() == 0 ? 1 : list.AsEnumerable().Max(p => p.Id) + 1;
+
         /// <summary>
         /// Get A List of Post
         /// </summary>
         [HttpGet]
-        public ActionResult<string> List()
-        {
-            return "A List";
-        }
+        public ActionResult<List<Post>> List() => list.ToList();
 
         /// <summary>
         /// Get a Post
         /// </summary>
         /// <param name="id">Post Id</param>
         [HttpGet("{id:int}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Post> Get(int id)
         {
-            return "An item";
+            var post = realm.Find<Post>(id);
+            if(post == null) return NotFound("Item not found");
+            return post;
         }
 
         /// <summary>
         /// Create a new Post
         /// </summary>
         [HttpPost]
-        public ActionResult<string> Post()
+        public async Task<ActionResult<Post>> Post()
         {
-            return "Post";
+            var post = new Post()
+            {
+                Id = NextId,
+                Content = "Content",
+                Title = "Title",
+                Created = new System.DateTimeOffset()
+            };
+            await realm.WriteAsync(r => post = r.Add(post));
+            return post;
         }
     }
 }
